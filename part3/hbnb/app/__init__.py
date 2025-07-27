df4ae1441849d1789e1b3aa7fca7be67b551adda
LouisManchon <10578@holbertonstudents.com>
2025-07-27 16:42:22 +0200

    fix error syntax

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    CORS(app, ressources={
+    CORS(app, resources={
         r"/*": {
             "origins": ["http://localhost:8000", "http://127.0.0.1:8000"],
             "methods": ["GET", "POST", "PUT", "DELETE"],
